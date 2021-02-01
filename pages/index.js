fdb82bbf6172006248355ae1fbf435c290d6cfc4
Christian Villamin <ChristianVillamin31@gmail.com>
2021-02-02 04:07:33 +0800

    update requests

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,32 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import request from "graphql-request";
 
-export default function Home({ featured }) {
-  console.log("featured:", featured);
-  const { posts } = featured;
+const build = (name) => {
+  const x = name.replace(/[^a-zA-Z]+/g, "");
+
+  return /* GraphQL */ `${x}:posts(
+    publicationState: LIVE
+    sort: "published_at:desc"
+    limit: 1
+    where: { category: { main_category: { name: "${name}" } } }
+  ) {
+    id
+    title
+    type
+    category {
+      main_category {
+        name
+      }
+    }
+    media {
+      formats
+    }
+  }`;
+};
+
+export default function Home(props) {
+  console.log("featured:", props);
+  const { posts } = props.featured;
 
   return (
     <div className={styles.container}>
@@ -72,6 +95,29 @@ export default function Home({ featured }) {
 }
 
 export async function getStaticProps() {
+  // --- Menu Query
+  const menu = await request(
+    `${process.env.STRAPI_API_URL}/graphql`,
+    /* GraphQL */ `
+      {
+        mainCategories(publicationState: LIVE) {
+          id
+          name
+        }
+        subCategories(publicationState: LIVE) {
+          id
+          name
+          main_category {
+            id
+            name
+          }
+        }
+      }
+    `
+  );
+
+  const mainCategories = menu.mainCategories.map((item) => item.name);
+
   const featured = await request(
     `${process.env.STRAPI_API_URL}/graphql`,
     /* GraphQL */ `
@@ -115,5 +161,109 @@ export async function getStaticProps() {
     `
   );
 
-  return { props: { featured }, revalidate: 1 };
+  // --- Latest Content
+  let buildQuery = "";
+
+  mainCategories.forEach((category) => {
+    const herApp = "Her App";
+    const courses = "Courses & Ebooks";
+
+    if (category === herApp || category === courses) return;
+
+    buildQuery += build(category);
+  });
+
+  const latest = await request(
+    `${process.env.STRAPI_API_URL}/graphql`,
+    `{${buildQuery}}`
+  );
+
+  // --- Her TV
+  const herTV = await request(
+    `${process.env.STRAPI_API_URL}/graphql`,
+    /* GraphQL */ `
+      {
+        posts(
+          publicationState: LIVE
+          sort: "published_at:desc"
+          limit: 8
+          where: { type: "Video" }
+        ) {
+          id
+          title
+          category {
+            main_category {
+              name
+            }
+          }
+          media {
+            formats
+          }
+        }
+      }
+    `
+  );
+
+  // --- Her Radio
+  const herRadio = await request(
+    `${process.env.STRAPI_API_URL}/graphql`,
+    /* GraphQL */ `
+      {
+        posts(
+          publicationState: LIVE
+          sort: "published_at:desc"
+          limit: 8
+          where: { type: "Podcast" }
+        ) {
+          id
+          title
+          tagline
+          category {
+            main_category {
+              name
+            }
+          }
+          media {
+            formats
+          }
+        }
+      }
+    `
+  );
+
+  // --- Trending
+  const trending = await request(
+    `${process.env.STRAPI_API_URL}/graphql`,
+    /* GraphQL */ `
+      {
+        posts(publicationState: LIVE, sort: "view_count:desc", limit: 15) {
+          id
+          title
+          type
+          tagline
+          category {
+            name
+            main_category {
+              slug
+            }
+          }
+          media {
+            formats
+          }
+        }
+      }
+    `
+  );
+
+  return {
+    props: {
+      menu,
+      featured,
+      latest,
+      herTV,
+      herRadio,
+      trending,
+    },
+    revalidate: 1,
+  };
 }
